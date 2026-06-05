5db0f43f9c7f97ee325e64a4645b2f96368e5b19
lucasszalves <souzaalves.lucas.2017@gmail.com>
2026-06-05 16:28:29 -0300

    initial commit

---
+++ merge_conflict_ex/teste.py
@@ -0,0 +1,11 @@
+def fibas(x):
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    return fibas(x-1) + fibas(x-2)
+
+print("git é mol lesgal")
+
+for n in range(0, 10):
+    print(fibas(n))
